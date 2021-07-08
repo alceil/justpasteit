743574fed25bb41d697bd4aa21b0a1b0737435ce
alceil <ashishshajisrampickal@gmail.com>
2021-07-08 16:20:48 +0530

    fixed type writter issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Typewriter from 'typewriter-effect';
 import style from "./Header.module.css";
 
 const Header = ({ headTitle }) => {
@@ -8,7 +8,21 @@ const Header = ({ headTitle }) => {
       <h1>{headTitle}📋</h1>
 
       <div data-aos="fade-right" className={style.typewriter}>
-      <h3>A pastebin alternative to share code/text with your friends.🧑‍💻</h3>
+        <h3>      <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString('A pastebin alternative to share code/text with your friends.🧑‍💻')
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .callFunction(() => {
+        console.log('All strings were deleted');
+      })
+      .start();
+  }}
+/></h3>
+
+      {/* <h3>A pastebin alternative to share code/text with your friends.🧑‍💻</h3> */}
 
       </div>
     </div>
