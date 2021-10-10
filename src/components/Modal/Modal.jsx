d1ae0875e2ec99b7cc4cef4bed7660e4f1ecb51c
tonalmathew <tonalmathew1999@gmail.com>
2021-10-10 12:41:22 +0530

    feat: make model responsive

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,65 +1,70 @@
-import React from 'react'
-import './Modal.css'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React from "react";
+import "./Modal.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 const MODAL_STYLES = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  border:'50px',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: 'var(--bg-color)',
-  padding: '50px',
-  zIndex: 1000
-}
+  position: "fixed",
+  // display: 'flex',
+  // flexFlow: 'column',
+  width: "50%",
+  top: "50%",
+  left: "50%",
+  border: "50px",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "var(--bg-color)",
+  padding: "50px",
+  zIndex: 1000,
+};
 
 const OVERLAY_STYLES = {
-  position: 'fixed',
+  position: "fixed",
   top: 0,
   left: 0,
   right: 0,
   bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
-  zIndex: 1000
-}
+  backgroundColor: "rgba(0, 0, 0, .7)",
+  zIndex: 1000,
+};
 
-export default function Modal({id, open, children, onClose }) {
+export default function Modal({ id, open, children, onClose }) {
   if (!open) return null;
   console.log(id);
   var url = `https://justpasteit.herokuapp.com/${id}`;
   const notify = () => toast("Link Copied📋");
   return (
     <>
-    <ToastContainer
-position="bottom-center"
-autoClose={1000}
-hideProgressBar={true}
-newestOnTop={true}
-closeOnClick
-pauseOnFocusLoss
-draggable
-pauseOnHover
-/>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div style={OVERLAY_STYLES} />
       <div style={MODAL_STYLES}>
+        <div className="modalTitle">
+          <h1 className="h1">Share it on Social Media📱</h1>
+        </div>
+        <div className="textarea_exp">
+          <p className="link">{url}</p>
+        </div>
 
-      <div className="modalTitle">
-          <h1>Share it on Social Media📱</h1>
-          </div>
-          <div className="textarea_exp">
-    <div className="link">{url}</div>
-    </div>
+        <div className="modalbutton">
+          <button className="end-buttons close-btn" onClick={onClose}>
+            Close
+          </button>
 
-    <div className="modalbutton">
-        <button className="end-buttons close-btn" onClick={onClose}>Close</button>
+          <CopyToClipboard text={url}>
+            <button onClick={notify} className="end-buttons copy-btn">
+              Copy
+            </button>
+          </CopyToClipboard>
 
-        <CopyToClipboard text={url}>
-  <button onClick={notify} className="end-buttons copy-btn" >Copy</button>
-</CopyToClipboard>
-
-     
-        {/* <button onClick={()=>{
+          {/* <button onClick={()=>{
                   window.open("https://www.facebook.com/sharer/sharer.php?u=","facebook-share-dialog","width=800,height=600")
 
               }} 
@@ -76,12 +81,9 @@ pauseOnHover
               className="end-buttons tweet-btn">
                   Tweet
               </button> */}
-              </div>
-              
-
+        </div>
       </div>
     </>
-  )
-    // document.getElementById('portal')
-  
+  );
+  // document.getElementById('portal')
 }
