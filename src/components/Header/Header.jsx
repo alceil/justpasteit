6478eeff6bfda46b05e4055ef9369eb8ea143473
TreyRuffy <TreyRuffy@users.noreply.github.com>
2021-10-17 23:25:55 -0600

    Emoji Fix
    Fixed using the JavaScript library: https://github.com/orling/grapheme-splitter
    Fixes #53
    
    Signed-off-by: TreyRuffy <TreyRuffy@users.noreply.github.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import Typewriter from 'typewriter-effect';
 import style from "./Header.module.css";
+import GraphemeSplitter from "grapheme-splitter"
+
+const stringSplitter = string => {
+  const splitter = new GraphemeSplitter();
+  return splitter.splitGraphemes(string);
+}
 
 const Header = ({ headTitle }) => {
   return (
-    <div  data-aos="fade-left" className={style.nav_container}>
+    <div data-aos="fade-left" className={style.nav_container}>
       <h1>📋 {headTitle}</h1>
 
       <div data-aos="fade-right" className={style.typewriter}>
         <h3>      <Typewriter
-  onInit={(typewriter) => {
-    typewriter.typeString('A pastebin alternative to share code/text with your friends.🧑‍💻')
-      .callFunction(() => {
-        console.log('String typed out!');
-      })
-      .pauseFor(2500)
-      .callFunction(() => {
-        console.log('All strings were deleted');
-      })
-      .start();
-  }}
-/></h3>
+          options={{
+            stringSplitter
+          }}
+          onInit={(typewriter) => {
+            typewriter.typeString('A pastebin alternative to share code/text with your friends.🧑💻')
+              .callFunction(() => {
+                console.log('String typed out!');
+              })
+              .pauseFor(2500)
+              .callFunction(() => {
+                console.log('All strings were deleted');
+              })
+              .start();
+          }}
+        /></h3>
 
-      {/* <h3>A pastebin alternative to share code/text with your friends.🧑‍💻</h3> */}
+        {/* <h3>A pastebin alternative to share code/text with your friends.🧑‍💻</h3> */}
 
       </div>
     </div>
